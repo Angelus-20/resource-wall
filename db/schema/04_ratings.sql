d117f9f9e7944aa882226fac4597ab92fc8208d9
SalmanHersi <hersi.salman@gmail.com>
2023-08-28 22:41:18 -0400

    create ratings bug

--- db/schema/04_ratings.sql
+++ db/schema/04_ratings.sql
@@ -2,6 +2,5 @@ DROP TABLE IF EXISTS ratings CASCADE;
 CREATE TABLE ratings (
   id SERIAL PRIMARY KEY NOT NULL,
   user_id INTEGER REFERENCES users(id) ON DELETE CASCADE,
-  -- deal_id INTEGER REFERENCES deals(id) ON DELETE CASCADE,
-  rating_value INTEGER CHECK (rating_value BETWEEN 1 AND 5)  -- Assuming a 1-5 rating system
+  rating_value INTEGER CHECK (rating_value BETWEEN 1 AND 5) 
 );
--- db/seeds/01_seeds.sql
+++ db/seeds/01_seeds.sql
@@ -4,8 +4,11 @@ INSERT INTO users (name, email, password) VALUES ('exampleUser1', 'exampleEmail1
 INSERT INTO users (name, email, password) VALUES ('exampleUser2', 'exampleEmail2@example.com', 'password2');
 INSERT INTO users (name, email, password) VALUES ('exampleUser3', 'exampleEmail3@example.com', 'password3');
 
+-- Change this from ratings, still having errors
 
-
+INSERT INTO liked (user_id, deal_id) VALUES (1,1);  
+INSERT INTO liked (user_id, deal_id) VALUES (2,1);
+INSERT INTO liked (user_id, deal_id) VALUES (3,1);
 
 
 INSERT INTO deals (user_id, title, description, URL) VALUES (1, 'titleTest1', 'descriptionTest1', 'exampleUrl1@example.com');
@@ -16,4 +19,10 @@ INSERT INTO deals (user_id, title, description, URL) VALUES (3, 'titleTest3', 'd
 INSERT INTO likes (user_id, deal_id) VALUES (1,1);
 INSERT INTO likes (user_id, deal_id) VALUES (2,1);
 INSERT INTO likes (user_id, deal_id) VALUES (3,1);
+
+
+INSERT INTO comments (user_id, deal_id, message) VALUES ( 1, 1, 'example message 1');
+INSERT INTO comments (user_id, deal_id, message) VALUES ( 2, 1, 'example message 2');
+INSERT INTO comments (user_id, deal_id, message) VALUES (3, 1, 'example message 3');
+
 
